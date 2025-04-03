ecd7a3d29ffd37fefca88e8e93a3c3ff319d916a
090602 <rowzfabre@gmail.com>
2025-04-03 21:59:15 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ extensions = [
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    'sphinx': ('https://www.w3schools.com/xml/default.asp', None),
 }
 intersphinx_disabled_domains = ['std']
 
